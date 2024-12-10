6e39b7bf33d04dbdf6f21deda5c0f423b4f4a816
nikolozi1VAR <m.atcharashvili.ia@gmail.com>
2024-12-10 21:16:36 +0400

    first commit

---
+++ Level 11/homework/leson.py
@@ -0,0 +1,15 @@
+#დავალება (1)
+num1 = int(input("enter a number: "))
+num2 = int(input("enter second number: "))
+if num1 == num2:
+    print(True)
+else:
+    print(False)
+#____________________________________________________________________________
+#დავალება (2)
+#num1 = int(input("enter your age: "))
+# num2 = 18
+# if num1 >= num2:
+#     print(True)
+# else:
+#     print(False)
