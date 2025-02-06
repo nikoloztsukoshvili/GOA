86e5a70cd02604393b37fe789dbc0ab9625c9668
nikoloztsukoshvili <n.tsukoshvili@buckswood.ge>
2025-02-06 21:33:25 +0400

    commit

--- Level 18/homework/lesson18.py
+++
@@ -1,9 +0,0 @@
-#1)
-#for i in range(1, 23, 2):
-#print(i)
-#2)
-#for i in range(12):
-#print("nikoloz")
-#3)
-#for i in range(0, 13, 2):
-#print(i)
--- level 27/classwork/.py
+++
@@ -1,9 +0,0 @@
-def check_alive(health):
-    if health > 0:
-        return True
-    elif health == 0:
-        return False
-    else:
-        return False
-
-print(check_alive(0))
