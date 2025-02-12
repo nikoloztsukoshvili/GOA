a18b6ca12ae5e2fa86d12df93b7f735fc07add78
nikoloztsukoshvili <n.tsukoshvili@buckswood.ge>
2025-02-12 20:58:49 +0400

    commit

---
+++ Level 17/classwork/index.py
@@ -0,0 +1,28 @@
+#1)
+# randomm = [1, 2, "4", 5, False, True, "abc", 8, 9, "ათი"]
+# print(randomm[0])
+# print(randomm[1])
+# print(randomm[2])
+
+# randomm[3] = False
+# randomm[4] = True
+# randomm[5] = "abcd"
+
+# randomm.append(9381247)
+# randomm.append(93812472653)
+# randomm.append(93812473846)
+# randomm.append(9381247329045602)
+# randomm.append(39872615671078514875689134813745698)
+# print(randomm)
+#2)=====================================================================
+user_number = []
+number = int(input('enter a number: '))
+user_number.append(number)
+while:True
+
+
+
+
+
+
+
