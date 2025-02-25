5d6c24415e6aaf2de6145ca42effd55949bbf121
nikoloztsukoshvili <n.tsukoshvili@buckswood.ge>
2025-02-25 20:23:37 +0400

    commit

---
+++ Level 19/classwork/classwork.py
@@ -0,0 +1,23 @@
+nums = [1,2,3,4,5,6]
+res = nums[::-1]
+print(res[2])
+print("answer is 4")
+
+word = str(input("enter any tipe of text: "))
+ress = word[-1]
+print(ress)
+#idk about that
+name = [1,2,3,4,5]
+name[1:-1]
+print(name)
+
+smthing = [1,2,3,4,5,6,7,8,9,10]
+print(smthing[4])
+print(smthing[5])
+print(smthing[6])
+print(smthing[7])
+print(smthing[8])
+print(smthing[9])
+
+ssmthing = str(input('enter any text again: '))
+print(ssmthing[::-1])
