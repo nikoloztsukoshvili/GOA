cba13e29ac8566be3c25f828bd0bd4d4737c7c5e
nikoloztsukoshvili <n.tsukoshvili@buckswood.ge>
2025-02-27 19:12:55 +0400

    commit

---
+++ Level 45/home/idk.py
@@ -0,0 +1,5 @@
+#მეტი ვერ გავიგე :(
+# print(int(input("enter num 1")) + int(input("enter num 2")))
+# print(int(input("enter num 1")) - int(input("enter num 2")))
+# print(int(input("enter num 1")) * int(input("enter num 2")))
+# print(int(input("enter num 1")) / int(input("enter num 2")))
